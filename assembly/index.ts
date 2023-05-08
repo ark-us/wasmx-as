a3e484054c75a8a333a1858a8bab9fff43f77dae
Loredana Cirstea <loredana.cirstea@gmail.com>
2023-05-08 22:25:21 +0300

    first working version of a simple store contract

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,42 +1,52 @@
-import { storageStore, storageStore2, storageStore3, storageLoad, getCallData, finish } from "./wasmx"
-// import { Uint8Array } from "assemblyscript/std/uint8array";
+import { JSON } from "json-as/assembly";
+import { storageStore, storageLoad, getCallData, finish, revert } from "./wasmx"
 
+@serializable
+class SetParams {
+  key!: string;
+  value!: string;
+}
+
+@serializable
+class GetParams {
+  key!: string;
+}
+
+@serializable
+class Calldata {
+  method!: string;
+  params!: string;
+}
 
 export function wasmx_env_1(): void {}
 
 export function instantiate(): void {}
 
 export function main(): void {
-  // const calld = getCallData()
-  const key = "hello"
-  const value = "sammy"
-  // storageStore("0", calld)
-  storageStore(key, value)
+  const calldata = getCallData();
+  const calldataJson = JSON.parse<Calldata>(String.UTF8.decode(calldata));
 
-  const keyEncoded = String.UTF8.encode(key);
-  const valueEncoded = String.UTF8.encode(value);
-  storageStore2(changetype<i32>(keyEncoded), keyEncoded.byteLength, changetype<i32>(valueEncoded), valueEncoded.byteLength)
-
-  storageStore3(keyEncoded, valueEncoded)
-
-  const newvalue = storageLoad(keyEncoded)
-  finish(newvalue)
+  if (calldataJson.method == "set") {
+    const args = JSON.parse<SetParams>(calldataJson.params);
+    set(args)
+    finish(new ArrayBuffer(0))
+  } else if(calldataJson.method == "get") {
+    const args = JSON.parse<GetParams>(calldataJson.params);
+    const result = get(args)
+    finish(result)
+  } else {
+    revert(String.UTF8.encode("method not found"))
+  }
 }
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+function set(args: SetParams): void {
+  const keyEncoded = String.UTF8.encode(args.key);
+  const valueEncoded = String.UTF8.encode(args.value);
+  storageStore(keyEncoded, valueEncoded);
 }
 
-
-
-// // Declare the start address of the string in memory
-// const address: i32 = /* memory address */;
-
-// // Declare the length of the string in bytes
-// const length: i32 = /* length of the string in bytes */;
-
-// // Read the bytes from memory into a Uint8Array
-// const bytes = Uint8Array.wrap(memory.buffer, address, length);
-
-// // Convert the Uint8Array to an AssemblyScript string
-// const str = String.fromUTF8Array(bytes);
+function get(args: GetParams): ArrayBuffer {
+  const keyEncoded = String.UTF8.encode(args.key);
+  const value = storageLoad(keyEncoded);
+  return value;
+}
--- assembly/wasmx.ts
+++ assembly/wasmx.ts
@@ -1,13 +1,8 @@
-// export declare function callDataCopy(ptr: i32, calldataPtr: i32, calldataLen: i32): void
-// export declare function storageStore(keyPtr: i32, keyLen: i32, valuePtr: i32, valueLen: i32): void
-// export declare function storageLoad(keyPtr: i32, keyLen: i32, valuePtr: i32): i32
-// export declare function finish(valuePtr: i32, valueLen: i32): void
-// export declare function revert(valuePtr: i32, valueLen: i32): void
-
-export declare function getCallData(): string
-export declare function storageStore(key: string, value: string): void
-export declare function storageStore2(keyPtr: i32, keyLen: i32, valuePtr: i32, valueLen: i32): void
-export declare function storageStore3(key: ArrayBuffer, value: ArrayBuffer): void
+export declare function getCallData(): ArrayBuffer
+export declare function storageStore(key: ArrayBuffer, value: ArrayBuffer): void
 export declare function storageLoad(key: ArrayBuffer): ArrayBuffer
+export declare function storageStoreGlobal(key: ArrayBuffer, value: ArrayBuffer): void
+export declare function storageLoadGlobal(key: ArrayBuffer): ArrayBuffer
+export declare function log(value: ArrayBuffer): void
 export declare function finish(value: ArrayBuffer): void
 export declare function revert(message: ArrayBuffer): void
